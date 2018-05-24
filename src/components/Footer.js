11eefe0e4b704e95aaad6a224c7f46a22c74e6f0
kdncode <kdncode@gmail.com>
2018-05-23 22:02:21 -0500

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,12 @@ import { Footer, FooterSection, FooterLinkList } from 'react-mdl';
 class Footer1 extends Component {
     render() {
         return (
-            <Footer size="mini" style={{ background: 'rgb(90, 255, 177)', color:  '#fff'}}>
-                <FooterSection type="left" logo="Title">
+            <Footer size="mini" style={{ background: '#f2f2f2', color:  '#555'}}>
+                <FooterSection type="left" logo="My Portfolio">
                     <FooterLinkList>
                         <a href="/">Help</a>
                         <a href="/">Privacy & Terms</a>
+                        <a href="/">2018 © Khoa Nguyen</a>
                     </FooterLinkList>
                 </FooterSection>
             </Footer>

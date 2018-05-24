d55bef425ab355f69c1ceacf9845b0d06cba1c09
kdncode <kdncode@gmail.com>
2018-05-23 22:58:56 -0500

    add Resume page

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,8 +6,24 @@ class Resume extends Component {
         return (
             <div className="contact-body">
                 <Grid className="contact-grid">
-                    <Cell col={6}>1</Cell>
-                    <Cell col={6}>2</Cell>
+                    <Cell col={4}>
+                        <div style={{textAlign: 'center'}}>
+                            <img src="https://cdn.dribbble.com/users/50579/screenshots/1631047/scalacat.png" alt="" />
+                        </div>
+                        <h2>Khoa Nguyen</h2>
+                        <h4>Software Engineer</h4>
+                        <hr />
+                        <h6>Address</h6>
+                        <p>123 st, Bay Area, CA 99999</p>
+                        <h6>Phone</h6>
+                        <p>(999) 999-9999</p>
+                        <h6>Email</h6>
+                        <p><a href="mailto:khoa.nguyen@engineer.com?Subject=Hello%20again"
+                            target="_top">khoa.nguyen@engineer.com</a></p>
+                    </Cell>
+                    <Cell col={8} className="resume-right-col">
+                        <h2>Education</h2>
+                    </Cell>
                 </Grid>
             </div>
         );
